67249a4ab33124066f732ad36b82478293edb368
LakiSaNakawBongBong! <91198430+Shiromorningstar@users.noreply.github.com>
2024-11-08 22:37:06 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 "yes"
+
+version '1.0.0'
+author 'Shiro Morningstar | https://discord.gg/Cb5Ag3kUXd'
+description 'sdw-helicopterScript | Discord: https://discord.gg/Cb5Ag3kUXd'
+
+
+client_scripts {'client/main.lua'}
